8e951f34a2f119c97f4d6a003d9a367b6a844fef
Rafał Majewski <goodheropl@gmail.com>
2023-04-25 09:27:07 +0200

    Support loading first name

--- src/contentSchema.ts
+++ src/contentSchema.ts
@@ -2,5 +2,8 @@ import Zod from "zod";
 
 const contentSchema = Zod.object({
 	gdprClause: Zod.string().min(1),
+	person: Zod.object({
+		firstName: Zod.string().min(1),
+	}),
 });
 export default contentSchema;
