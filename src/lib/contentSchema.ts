c3ddc67afa5ac6fd42df0373f195909d88364b47
Rafał Majewski <goodheropl@gmail.com>
2023-08-20 19:09:23 +0200

    Lowercase zod import

--- src/lib/contentSchema.ts
+++ src/lib/contentSchema.ts
@@ -1,51 +1,51 @@
-import Zod from "zod";
+import zod from "zod";
 
-export const contactEntrySchema = Zod.object({
-	datum: Zod.string().min(1),
-	href: Zod.string().min(1),
-	icon: Zod.object({
-		filepath: Zod.string().min(1),
-		alt: Zod.string().min(1),
+export const contactEntrySchema = zod.object({
+	datum: zod.string().min(1),
+	href: zod.string().min(1),
+	icon: zod.object({
+		filepath: zod.string().min(1),
+		alt: zod.string().min(1),
 	}),
 });
 
-export const educationEntrySchema = Zod.object({
-	name: Zod.string().min(1),
-	period: Zod.object({
-		start: Zod.string().min(1),
-		end: Zod.string().min(1),
+export const educationEntrySchema = zod.object({
+	name: zod.string().min(1),
+	period: zod.object({
+		start: zod.string().min(1),
+		end: zod.string().min(1),
 	}),
-	description: Zod.string().min(1),
-	href: Zod.string().min(1),
-	icon: Zod.object({
-		filepath: Zod.string().min(1),
+	description: zod.string().min(1),
+	href: zod.string().min(1),
+	icon: zod.object({
+		filepath: zod.string().min(1),
 	}),
-	additionalInformations: Zod.array(Zod.string().min(1)),
+	additionalInformations: zod.array(zod.string().min(1)),
 });
 
-export const knownTechnologySchema = Zod.object({
-	name: Zod.string().min(1),
-	icon: Zod.object({
-		filepath: Zod.string().min(1),
+export const knownTechnologySchema = zod.object({
+	name: zod.string().min(1),
+	icon: zod.object({
+		filepath: zod.string().min(1),
 	}),
-	details: Zod.string().min(1),
+	details: zod.string().min(1),
 });
 
-const contentSchema = Zod.object({
-	gdprClause: Zod.string().min(1),
-	person: Zod.object({
-		firstName: Zod.string().min(1),
-		lastName: Zod.string().min(1),
-		description: Zod.string().min(1),
-		contact: Zod.object({
+const contentSchema = zod.object({
+	gdprClause: zod.string().min(1),
+	person: zod.object({
+		firstName: zod.string().min(1),
+		lastName: zod.string().min(1),
+		description: zod.string().min(1),
+		contact: zod.object({
 			phoneNumber: contactEntrySchema,
 			emailAddress: contactEntrySchema,
 			location: contactEntrySchema,
 			linkedin: contactEntrySchema,
 			github: contactEntrySchema,
 		}),
-		education: Zod.array(educationEntrySchema),
-		knownTechnologies: Zod.array(knownTechnologySchema),
+		education: zod.array(educationEntrySchema),
+		knownTechnologies: zod.array(knownTechnologySchema),
 	}),
 });
 export default contentSchema;
